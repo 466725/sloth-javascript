5193a85de367bbc66a7d02041db1ee71cf483dc6
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-03-16 14:53:57 -0400

    Update homepage.spec.ts

--- cypress/integration/homepage.spec.ts
+++ cypress/integration/homepage.spec.ts
@@ -10,13 +10,21 @@ describe("Tangerine Homepage", () => {
     home.getLogo().should("be.visible");
   });
 
+  it("should display 'Get Our App' links", () => {
+    home.visit();
+    // Scroll to the bottom of the page
+    cy.scrollTo('bottom');
+    // Check if the 'Get Our App' links for iOS and Android are visible
+    cy.get('#ios-app-store-link').should('be.visible');
+    cy.get('#android-app-store-link').should('be.visible');
+  });
+
   it("should capture analytics events", () => {
     const analyticsEvents: any[] = [];
     cy.intercept('POST', '**google-analytics.com/g/collect', (req) => {
       analyticsEvents.push(req.body);
     });
-    cy.visit('https://www.tangerine.ca/en/personal');
-    cy.acceptCookies();
+    home.visit();
     cy.wait(3000);   // allow multiple analytics events
     cy.then(() => {
       expect(analyticsEvents.length).to.be.greaterThan(-1);
