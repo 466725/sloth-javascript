8ab9d7f832954b6f7a142d1d44bf26b7a95e2b51
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-07-19 20:36:58 -0400

    Create register.cy.ts

---
+++ cypress/tests/register.cy.ts
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+import { HomePage } from "../support/pages/HomePage";
+
+describe("Tangerine Registration", () => {
+  const home = new HomePage();
+
+  beforeEach(() => {
+    home.visit();
+  });
+
+  it("should verify the Sign Up page loads", () => {
+    cy.allure().feature('Registration');
+    cy.allure().story('Validate registration page loads correctly');
+    // The site has multiple "Become a Client" links, some hidden for mobile.
+    // We target a visible link ('a:visible') to ensure we click the correct one.
+    cy.get("a:visible").contains(/Sign Up|Become a Client/i).should("be.visible");
+  });
+});
