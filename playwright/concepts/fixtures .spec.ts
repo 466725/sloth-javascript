3d4c08bb755f191c853d135794dc218b08f384f2
Weipeng Zheng <weipeng.zheng.ca@gmail.com>
2026-07-20 20:17:31 -0400

    Update fixtures .spec.ts

--- playwright/concepts/fixtures .spec.ts
+++ playwright/concepts/fixtures .spec.ts
@@ -2,6 +2,7 @@ import { expect, test as base } from '@playwright/test';
 
 type DemoFixtures = {
   message: string;
+  decoratedMessage: string;
 };
 
 const test = base.extend<DemoFixtures>({
@@ -14,8 +15,16 @@ const test = base.extend<DemoFixtures>({
 
     // Teardown (nothing needed for this simple demo)
   },
+  decoratedMessage: async ({ message }, use) => {
+    // This fixture is composed from another fixture.
+    await use(`*** ${message} ***`);
+  },
 });
 
 test('uses a custom fixture value', async ({ message }) => {
   expect(message).toBe('Hello from fixture');
+});
+
+test('uses a fixture that depends on another fixture', async ({ decoratedMessage }) => {
+  expect(decoratedMessage).toBe('*** Hello from fixture ***');
 });
